62ad9b575c8ba1d866fe19b5c5661b7ea2595a4d
Yago Siqueira <yagosiqti@gmail.com>
2022-03-23 09:59:48 -0300

    Exercicio 02 com FOR

---
+++ Exercicio02a.java
@@ -0,0 +1,10 @@
+// 2. Reimplemente o exercício 1, fazendo com 
+//que os valores sejam exibidos em ordem reversa.
+//=================USANDO FOR==============================
+public class Exercicio02a {
+    public static void main(String[] args) {
+        for(int contador=100; contador >= 0; contador--){
+            System.out.printf(contador + " ");     
+        }
+    }
+}
