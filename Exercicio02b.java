0ecb2f979f6f03fd1ff1fe151beb27df0b07d48a
Yago Siqueira <yagosiqti@gmail.com>
2022-03-23 10:00:19 -0300

    Exercicio 02 com WHILE

---
+++ Exercicio02b.java
@@ -0,0 +1,9 @@
+public class Exercicio02b {
+    public static void main(String[] args) {
+        int contador=100;
+        while(contador>=0){
+            System.out.printf(contador + " ");
+            contador--;
+        }
+    }
+}
