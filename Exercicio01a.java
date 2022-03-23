53938c46ddaa3d4b74a6ac793973b0b22fcf105e
Yago Siqueira <yagosiqti@gmail.com>
2022-03-23 09:57:12 -0300

    Exercicio 01 com FOR

---
+++ Exercicio01a.java
@@ -0,0 +1,11 @@
+//1) Escreva um programa que exiba os primeiros 100 números
+//naturais. Faça versões usando for, whilhe e do while.
+//=================USANDO FOR==============================
+
+public class Exercicio01a{
+    public static void main(String[] args) {
+        for(int contador=0; contador <=100; contador++){
+            System.out.printf(contador + " ");
+        }
+    }
+}
