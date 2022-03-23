087557703cfde967b702faf6d20e080e005cb7e6
Yago Siqueira <yagosiqti@gmail.com>
2022-03-23 09:59:10 -0300

    Exercicio 01 com DO WHILE

---
+++ Exercicio02c.java
@@ -0,0 +1,9 @@
+public class Exercicio02c {
+    public static void main(String[] args) {
+        int contador = 100;
+        do{
+            System.out.println(contador + " ");
+            contador--;
+        }while(contador >=0);
+    }
+}
